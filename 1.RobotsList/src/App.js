ade1f17913b1dc06a760b1faf3c53da20130920a
Btrnq <b.tronquoy@gmail.com>
2018-08-13 14:06:56 +0200

    tried something

--- 1.RobotsList/src/App.js
+++ 1.RobotsList/src/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import Searchbox from './Searchbox';
 import { robots } from './robots.js';
 
-const App = () => {
-    return (
-        <div className='tc'>
-            <h1>RobotsList</h1>
-            <Searchbox />
-            <CardList robots={robots} />
-        </div>
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
 
-    );
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value});
+        console.log('yes ' + event.target.value);
+        const filteredRobots = this.state.robots.filter(robots =>{
+            return robots.name.toLowerCase().includes(this.searchfield.toLowerCase());
+        })
+    }
+    render () {
+        return (
+            <div className='tc'>
+                <h1>RobotsList</h1>
+                <Searchbox searchChange={this.onSearchChange} />
+                <CardList robots={this.state.robots} />
+            </div>
+    
+        );
+    }
 }
 
 export default App;
--- 1.RobotsList/src/Searchbox.js
+++ 1.RobotsList/src/Searchbox.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const Searchbox = () => {
+const Searchbox = ({ searchField, searchChange }) => {
     return (
         <div className="pa2">
         <input 
             className='pa3 ba b--blue bg-lightest-blue'
             type='search' 
-            placeholder='search some robot.. ;)' 
+            placeholder='search some robots.' 
+            onChange={searchChange}
             />
         </div>
     );
